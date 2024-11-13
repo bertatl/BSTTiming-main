4299dd52835b44a0bb41399391bdd6b27193ce87
NoReply Amazon <no-reply@amazon.com>
2024-11-13 22:44:03 +0000

    Revert "[Commit] Iteration 652 #errors = 137 <= 136."
    This reverts commit 54dfec76b6454e295746d0e7301b288032cdcd02.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace BSTTiming
 {
-    public class Program
+    public class Program : System.Object
     {
         /// <summary>
         /// Duration of one second
