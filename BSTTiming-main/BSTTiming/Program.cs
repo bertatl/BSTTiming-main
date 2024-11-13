3b73b239a263159175626e71356af66ab85aa8ed
NoReply Amazon <no-reply@amazon.com>
2024-11-13 23:00:14 +0000

    Revert "[Commit] Iteration 690 #errors = 140 <= 140."
    This reverts commit a361a63d8f1f5a79b4ceff317c4146ff78630c78.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -47,7 +47,7 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             int[] items = GenerateSearchItems(1024);
 
             // Create a stopwatch
-            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            Stopwatch sw = new Stopwatch();
 
             Random random = new Random();
 
@@ -134,7 +134,7 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
         /// <summary>
         /// Returns the number of milliseconds that have elapsed on the Stopwatch.
         /// </summary>
-        public static double msecs(System.Diagnostics.Stopwatch sw)
+        public static double msecs(Stopwatch sw)
         {
             return (((double)sw.ElapsedTicks) / Stopwatch.Frequency) * 1000;
         }
