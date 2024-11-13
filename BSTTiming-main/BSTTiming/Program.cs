1d7b82fed6c2b5ebe4c110c4086822e6c2fce41b
NoReply Amazon <no-reply@amazon.com>
2024-11-13 23:37:23 +0000

    [Commit] Iteration 772 #errors = 136 <= 143.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0246][44,23] The type or namespace name 'System' could not be found (are you missing a using directive or an assembly reference?)``.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 
 namespace BSTTiming
 {
-    public class Program : System.Object
+    public class Program
     {
         /// <summary>
         /// Duration of one second
@@ -16,13 +12,10 @@ namespace BSTTiming
 
         public static int SIZE;
 
-        // Explicitly declare string type
-        public static System.String line;
-
         public static void Main(string[] args)
         {
-            System.String line;
-using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
+            string line;
+using (StreamWriter file = new StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
             {
                 line = "Time";
                 Console.WriteLine(line);
@@ -41,7 +34,7 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             Console.Read();
         }
 
-        public static System.Double RunBSTTiming(int size)
+        public static double RunBSTTiming(int size)
         {
             // Construct a randomly-generated balanced
             //binary search tree
