000d070e6c95b7b01a7a9ead6325ba48eb212afe
NoReply Amazon <no-reply@amazon.com>
2024-11-13 23:45:24 +0000

    [Commit] Iteration 791 #errors = 144 <= 144.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0518][24,33] Predefined type 'System.String' is not defined or imported``.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad1edf4a47a4aa1ce3d2eeb2c75bdcf01445181b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf3cbe3b8486b202b53dafb950cf7b470b72723")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
