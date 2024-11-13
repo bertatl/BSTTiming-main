62d2b24643f85f60a082d33eed1867bff793f86d
NoReply Amazon <no-reply@amazon.com>
2024-11-13 22:53:38 +0000

    [Commit] Iteration 675 #errors = 140 <= 140.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0246][115,24] The type or namespace name 'SortedSet<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8719910cc096a0223960849fd8daf7631e5401")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a55f6aeddab395ffba9f1e5189f6b4b8f5d390c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
