159348ceae18d70051c36a0bd04aea79274b515d
NoReply Amazon <no-reply@amazon.com>
2024-11-13 22:33:56 +0000

    [Commit] Iteration 625 #errors = 141 <= 140.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0518][115,52] Predefined type 'System.Int32' is not defined or imported``.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 
 namespace BSTTiming
 {
-    public class Program : System.Object
+    public class Program
     {
         /// <summary>
         /// Duration of one second
