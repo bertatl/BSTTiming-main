b60d611542b5146d989c437601ed06601d0a0d51
NoReply Amazon <no-reply@amazon.com>
2024-11-13 23:07:01 +0000

    Revert "[Commit] Iteration 705 #errors = 142 <= 141."
    This reverts commit 9ef62c97067d529751b0ad4b0d7a90527c0f09a8.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e667d0bd392e5a2ba54eb308e0d1c438e76f6712")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+265146cffa18b18de87649e702d21ff3730d6abe")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
