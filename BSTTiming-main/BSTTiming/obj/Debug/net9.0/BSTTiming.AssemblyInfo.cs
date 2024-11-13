05fde0e50604478d0e6ff34dde71908987525301
NoReply Amazon <no-reply@amazon.com>
2024-11-13 17:32:08 +0000

    Revert "[Commit] Iteration 19 #errors = 143 <= 143."
    This reverts commit bee51287de3f604910a8a9e7f4f977dfe2b022de.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f5c0bd78047e76c7436b0e996404d69958efcce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d2d6b322d94c0eeaf7346c665687958892a1a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
