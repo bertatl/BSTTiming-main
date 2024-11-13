d186b1876abcd08b926707570a08c004c73f9b04
NoReply Amazon <no-reply@amazon.com>
2024-11-13 18:47:33 +0000

    [Commit] Iteration 156 #errors = 145 <= 147.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0518][18,22] Predefined type 'System.Object' is not defined or imported``.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01220adbf7ba68d63bdca129812b6da0029c049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126abbdf7277d12cbdeb30e341b126d33c6f520c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
