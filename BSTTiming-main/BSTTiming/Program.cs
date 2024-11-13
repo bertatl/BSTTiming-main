3ff2bf6c4168552fa81c65a37d56afeb3c4e85fd
NoReply Amazon <no-reply@amazon.com>
2024-11-13 23:26:58 +0000

    [Commit] Iteration 750 #errors = 143 <= 140.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0518][21,13] Predefined type 'System.String' is not defined or imported``.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -16,9 +16,12 @@ namespace BSTTiming
 
         public static int SIZE;
 
+        // Explicitly declare string type
+        public static System.String line;
+
         public static void Main(string[] args)
         {
-            string line;
+            System.String line;
 using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
             {
                 line = "Time";
