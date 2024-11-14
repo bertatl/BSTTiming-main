efcc6b1422926016a235fac81a63b999419920e4
NoReply Amazon <no-reply@amazon.com>
2024-11-14 01:17:37 +0000

    [Commit] Iteration 1009 #errors = 141 <= 140.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0518][24,8] Predefined type 'System.Object' is not defined or imported``.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -7,7 +7,7 @@ using System.Linq;
 
 namespace BSTTiming
 {
-    public class Program : System.Object
+    public class Program
     {
         /// <summary>
         /// Duration of one second
@@ -21,7 +21,7 @@ namespace BSTTiming
 
         public static void Main(string[] args)
         {
-using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
+using (StreamWriter file = new StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
             {
                 line = "Time";
                 Console.WriteLine(line);
@@ -37,7 +37,7 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
                 }
             }
             Console.WriteLine("Finished");
-            Console.Read();
+            Console.ReadLine();
         }
 
         public static double RunBSTTiming(int size)
