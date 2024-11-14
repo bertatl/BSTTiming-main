cf86b83ff0490bcb06e30a9a7469ac34ae9f7f85
NoReply Amazon <no-reply@amazon.com>
2024-11-14 00:47:57 +0000

    [Commit] Iteration 940 #errors = 150 <= 142.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0518][118,34] Predefined type 'System.Int32' is not defined or imported``.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -12,9 +12,9 @@ namespace BSTTiming
         /// <summary>
         /// Duration of one second
         /// </summary>
-        public const int DURATION = 1000;
+        public const System.Int32 DURATION = 1000;
 
-        public static int SIZE;
+        public static System.Int32 SIZE;
 
         // Explicitly declare string type
         public static string line;
@@ -115,13 +115,13 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             return set.ToArray();
         }
 
-        private static SortedSet<int> GenerateTree(int size)
+        private static SortedSet<System.Int32> GenerateTree(System.Int32 size)
         {
-            SortedSet<int> bst = new SortedSet<int>();
+            SortedSet<System.Int32> bst = new SortedSet<System.Int32>();
             Random random = new Random();
 
-            int number;
-            for (int i = 0; i < size; i++)
+            System.Int32 number;
+            for (System.Int32 i = 0; i < size; i++)
             {
                 do
                 {
@@ -130,7 +130,7 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
 
                 bst.Add(number);
             }
-            
+
             return bst;
         }
 
