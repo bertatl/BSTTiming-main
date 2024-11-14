44ad621e09d386f2dffce3ac82e2da4ad4967221
NoReply Amazon <no-reply@amazon.com>
2024-11-14 01:55:16 +0000

    Revert "[Commit] Iteration 1091 #errors = 141 <= 141."
    This reverts commit 9b52f39c1905357694afd7de9e274cf1487d9a3e.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -1,10 +1,10 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Collections.Immutable;
-global using System.Diagnostics;
-global using System.IO;
-global using System.Linq;
-global using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace BSTTiming
 {
