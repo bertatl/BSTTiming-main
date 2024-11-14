d9670af6d2a0d3d8f62f6559585fbc6cc6001645
NoReply Amazon <no-reply@amazon.com>
2024-11-14 01:41:23 +0000

    Revert "[Commit] Iteration 1060 #errors = 142 <= 141."
    This reverts commit 6db6210707407d13d7f76f754d119a3d861bfcb4.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -1,14 +1,14 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Collections.Immutable;
-global using System.Diagnostics;
-global using System.IO;
-global using System.Linq;
-global using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace BSTTiming
 {
-    public class Program
+    public class Program : System.Object
     {
         /// <summary>
         /// Duration of one second
