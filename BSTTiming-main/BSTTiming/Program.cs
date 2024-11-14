15192ab9cac3f7e64ff33bea4f85ba2ba663da46
NoReply Amazon <no-reply@amazon.com>
2024-11-14 01:08:51 +0000

    Revert "[Commit] Iteration 989 #errors = 158 <= 140."
    This reverts commit 7e43f64bdb6bd767554506960093c40afa42ba85.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -12,12 +12,12 @@ namespace BSTTiming
         /// <summary>
         /// Duration of one second
         /// </summary>
-        public const System.Int32 DURATION = 1000;
+        public const int DURATION = 1000;
 
-        public static System.Int32 SIZE;
+        public static int SIZE;
 
         // Explicitly declare string type
-        public static System.String line;
+        public static string line;
 
         public static void Main(string[] args)
         {
@@ -40,13 +40,13 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             Console.Read();
         }
 
-        public static double RunBSTTiming(System.Int32 size)
+        public static double RunBSTTiming(int size)
         {
             // Construct a randomly-generated balanced
             //binary search tree
-            SortedSet<System.Int32> bst = GenerateTree(size);
+            SortedSet<int> bst = GenerateTree(size);
 
-            System.Int32[] items = GenerateSearchItems(1024);
+            int[] items = GenerateSearchItems(1024);
 
             // Create a stopwatch
             Stopwatch sw = new Stopwatch();
@@ -97,12 +97,12 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             return (totalAverage - overheadAverage) / 1024;
         }
 
-        private static System.Int32[] GenerateSearchItems(System.Int32 size)
+        private static int[] GenerateSearchItems(int size)
         {
-            HashSet<System.Int32> set = new HashSet<System.Int32>();
+            HashSet<int> set = new HashSet<int>();
             Random random = new Random();
-            System.Int32 num;
-            for(System.Int32 i = 0; i < size; i++)
+            int num;
+            for(int i = 0; i < size; i++)
             {
                 do
                 {
@@ -114,13 +114,13 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             return set.ToArray();
         }
 
-        private static SortedSet<System.Int32> GenerateTree(System.Int32 size)
+        private static SortedSet<int> GenerateTree(int size)
         {
-            SortedSet<System.Int32> bst = new SortedSet<System.Int32>();
+            SortedSet<int> bst = new SortedSet<int>();
             Random random = new Random();
 
-            System.Int32 number;
-            for (System.Int32 i = 0; i < size; i++)
+            int number;
+            for (int i = 0; i < size; i++)
             {
                 do
                 {
@@ -129,7 +129,7 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
 
                 bst.Add(number);
             }
-
+            
             return bst;
         }
 
